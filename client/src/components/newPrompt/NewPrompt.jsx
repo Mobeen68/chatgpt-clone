abca7740277c56d572eb0992481e5bc04fe80a21
Mobeen Ejaz <mobeene98@gmail.com>
2024-10-11 20:51:40 +0500

    AI chat implemented

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,22 +1,85 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [prompt, setPrompt] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const [image, setImage] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
+  });
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+  });
+
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  }, [answer, prompt, image.dbData]);
+
+  const add = async (params) => {
+    setPrompt(params);
+
+    const result = await chat.sendMessage(
+      Object.entries(image.aiData).length > 0 ? [image.aiData, params] : params
+    );
+    setAnswer(result.response.text());
+    setImage({
+      isLoading: false,
+      error: "",
+      dbData: {},
+      aiData: {},
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+  };
 
   return (
     <>
+      {image.isLoading && <p>Loading...</p>}
+      {image.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={image.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
+      {prompt && <div className="message user">{prompt}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="endChat" ref={endRef}></div>
-      <form action="" className="newForm">
-        <label htmlFor="file">
-          <img src="/attachment.png" alt="" />
-        </label>
+      <form action="" className="newForm" onSubmit={handleSubmit}>
+        <Upload setImage={setImage} />
         <input type="file" name="" multiple={false} id="file" hidden />
-        <input type="text" placeholder="Ask anything..." />
+        <input type="text" name="text" placeholder="Ask anything..." />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
